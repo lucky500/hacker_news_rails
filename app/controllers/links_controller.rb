988839f1169883d0a5d7b2fba2c2196676356c66
Luciano Oliveira <luciano101@msn.com>
2017-11-14 23:31:48 -0800

    user able to edit and delete submitted links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -23,15 +23,34 @@ class LinksController < ApplicationController
   end
 
   def edit
-    @link = Link.find_by(id: params[:id])
+    link = Link.find_by(id: params[:id])
 
     if current_user.owns_link?(link)
-      @link
-      #@link = link
+      @link = link
     else
       redirect_to root_path, notice: 'Not authorized to edit this link'
     end
   end
+
+  def update
+    @link = current_user.links.find_by(id: params[:id])
+    if @link.update(link_params)
+      redirect_to root_path, notice: 'Link was successfully updated'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    link = Link.find_by(id: params[:id])
+    if current_user.owns_link?(link)
+      link.destroy
+      redirect_to root_path, notice: 'Link successfully deleted'
+    else
+      redirec_to root_path, notice: 'You are not authorized to delete this link'
+    end
+  end
+
 
   private
 
