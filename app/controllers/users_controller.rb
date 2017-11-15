3fad435e8cd7809d6963f64cdaa4f417dc6cceca
Luciano Oliveira <luciano101@msn.com>
2017-11-14 19:39:18 -0800

    allow logged in user to enter a link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_back fallback_location: new_user_path
+      login(@user)
+      redirect_to root_path, notice: 'Logged in'
     else
       render :new
     end
